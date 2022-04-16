b7b8d187ebe5dda7e656332dbf146369304adf39
201122771 <unconfigured@null.spigotmc.org>
2022-04-16 02:44:58 -0700

    v2.20.4
    MORE QUOTESS YOOO

--- pages/homepage/js/script.js
+++ pages/homepage/js/script.js
@@ -5,7 +5,26 @@ const quotes =
     "You were the aggressor in this relationship.",
     "You need to discipline them until they're good at it.",
     "What goes up, must come down.",
-    "You're just not independent, Matthew."
+    "You're just not independent, Matthew.",
+    "I never drifted away, it was your excuse.",
+    "Power always gets out the worse in you.",
+    "Egg Grenade deserves all the negativity but death.",
+    "Ms. Bachelier is sexist without wanting to admit it.",
+    "Homophobia doesn't involve anything with fear.",
+    "Free Hong Kong",
+    "L Russia, Ukraine on top 💪💪",
+    "Christopher is a bozo 💀",
+    "Sombat is a bozo 💀",
+    "Aaden is goofy af 💀",
+    "Thiery is racist 💀",
+    "Auctioning off Jacob for $100,000.",
+    "Luz Marie is amazing :D",
+    "THANK YOUU ANGELINAA <33",
+    "HELLOO XITLALYY!!",
+    "YOO HELLO JUSTINN :D",
+    "Matthew Patthew am I right??",
+    "NeoXephox on top 💪💪",
+    "HELLOO NEO SAVANN :D"
 ];
 
 function randomQuote() {
