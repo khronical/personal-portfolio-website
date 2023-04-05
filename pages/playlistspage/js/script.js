255a25ae6fe52f17923469135be0e15925ee0784
201122771 <unconfigured@null.spigotmc.org>
2023-04-05 00:55:51 -0700

    Update script.js

--- pages/playlistspage/js/script.js
+++ pages/playlistspage/js/script.js
@@ -50,3 +50,36 @@ document.addEventListener("DOMContentLoaded", () => {
 
     playlistsContainer.innerHTML = '';
 });
+
+// THEME CODING SHIT
+let theme = window.localStorage.getItem('likes-sun', 'yes');
+let scheme = window.matchMedia(`(prefers-color-scheme: light)`);
+
+if (theme === 'no') {
+	document.body.classList.add('dark');
+    console.log('client hates the sun :(');
+} else if (theme === 'yes') {
+	document.body.classList.remove('dark');
+    console.log('client likes the sun :D');
+} else {
+	if (scheme) {
+        document.body.classList.remove('dark');
+		console.log('client is new, but prefers light');
+	} else {
+		document.body.classList.add('dark');
+		console.log('client is new, but prefers dark');
+	}
+}
+
+function toggleTheme() {
+    document.body.classList.toggle('dark')
+
+    if (document.body.classList.contains('dark')) {
+		window.localStorage.setItem('likes-sun', 'no');
+		console.log('client likes emo');
+	} else {
+		window.localStorage.setItem('likes-sun', 'yes');
+		console.log('client likes sunlight');
+	}
+}
+// THEME CODING SHIT END
