0fd6d9a02f7959953a1bac8ec24593a35178fcb2
Bryan Betanzos <79638827+201122771@users.noreply.github.com>
2025-08-13 21:48:27 -0700

    iteration 6 part 1

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_DszqSjpU.mjs';
-import { manifest } from './manifest_D8Mas2Da.mjs';
+import { manifest } from './manifest_Bt9GIrWl.mjs';
 
 const serverIslandMap = new Map();;
 
