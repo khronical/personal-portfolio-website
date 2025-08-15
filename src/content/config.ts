695be2c8a6309fb05f9e7ca4523c19924afb0118
Bryan Betanzos <79638827+201122771@users.noreply.github.com>
2025-08-15 10:23:40 -0700

    Added Journals

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,16 @@
 // content.config.mjs
 import { ObsidianDocumentSchema, ObsidianMdLoader } from "astro-loader-obsidian";
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 
 export const collections = {
 	posts: defineCollection({
 		loader: ObsidianMdLoader({
 			base: 'src/content/journal',
-			url: 'docs',
+			url: 'journal',
 		}),
 		schema: ({ image }) => ObsidianDocumentSchema.extend({
 			image: image().optional(),
-			cover: image().optional(),
+			description: z.string().optional(),
 		}),
 	}),
 };
