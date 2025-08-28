7b90e2fcdd2383ab492014b69506cb5ff2c9bb04
Bryan Betanzos <79638827+201122771@users.noreply.github.com>
2025-08-28 05:41:05 -0700

    Small changes

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_D27g2GHa.mjs';
-import { manifest } from './manifest_DHPsJYjb.mjs';
+import { manifest } from './manifest_CcAjaGVx.mjs';
 
 const serverIslandMap = new Map();;
 
