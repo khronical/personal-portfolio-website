c56c84ffd1ff5941e9d68baf95f76775aae3fda2
Bryan Betanzos <79638827+201122771@users.noreply.github.com>
2025-08-29 20:37:14 -0700

    wowowow open source part 2

---
+++ README.md
@@ -0,0 +1,4 @@
+# Personal Portfolio Website
+This repository is the entire source to my website, [Khronical.dev](https://khronical.dev/)!
+
+I'm not gonna bother going through how to set everything up, just know its built with the Astro Framework then deployed to cloudflare
--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_D27g2GHa.mjs';
-import { manifest } from './manifest_CcAjaGVx.mjs';
+import { manifest } from './manifest_LqdbAtns.mjs';
 
 const serverIslandMap = new Map();;
 
