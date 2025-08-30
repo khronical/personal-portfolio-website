f74257f6245f7a1efb2dcfdf5d5ee4c5973fd83d
Bryan Betanzos <79638827+201122771@users.noreply.github.com>
2025-08-29 22:54:36 -0700

    added random web buttons xD

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_D27g2GHa.mjs';
-import { manifest } from './manifest_LqdbAtns.mjs';
+import { manifest } from './manifest_gNV4UvuK.mjs';
 
 const serverIslandMap = new Map();;
 
